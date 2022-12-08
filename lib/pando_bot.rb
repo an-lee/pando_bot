a5a2827493c90783ae93701483690c712c441326
an-lee <an.lee.work@gmail.com>
2022-12-08 17:08:19 +0800

    init

---
+++ lib/pando_bot.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "pando_bot/version"
+
+module PandoBot
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/pando_bot/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module PandoBot
+  VERSION = "0.1.0"
+end
